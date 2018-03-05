7bf50f8d0186bd1dd8281047283df5dfd2ac75c5
donovan122@yahoo.com <donovan122@yahoo.com>
2018-03-05 13:57:04 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 var cards =["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("user fipped" +" "+cardsInPlay[0]);
-console.log("user fipped" +" "+cardsInPlay[1]);
-if (cardsInPlay.length === 2) 
+var checkForMatch = function(){
+		if (cardsInPlay.length === 2) 
 	{if (cardsInPlay[0]===cardsInPlay[1]) {alert("You found a match!")}
-	else {alert("Sorry, try again." )}};
+	else {alert("Sorry, try again.")}};
+}
+var flipCard = function(cardID) {
+	console.log("user flipped"+ " " + cards[cardID])
+	cardsInPlay.push(cards[cardID])
+	checkForMatch()
+};
+flipCard(0);
+flipCard(2);
 	
